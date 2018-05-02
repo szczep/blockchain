01fbf54e005f39b8ca7c474a1b30a5a0ba1eac0c
Szczepan <Q1N9@nykredit.dk>
2018-05-02 14:04:21 +0200

    initiall commit with basic block chain hash check

---
+++ src/main/java/pl/szczep/model/BlockChainUtil.java
@@ -0,0 +1,27 @@
+package pl.szczep.model;
+
+
+import java.util.List;
+
+public class BlockChainUtil {
+
+
+    public static Boolean isChainValid(List<Block> blockchain) {
+        Block currentBlock;
+        Block previousBlock;
+
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/pl/szczep/model/BlockChainUtilTest.java
@@ -0,0 +1,36 @@
+package pl.szczep.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class BlockChainUtilTest {
+
+
+    @Test
+    public void shouldBlockChainBeValid() {
+
+        List<Block> validBlockChain = new ArrayList<Block>();
+        validBlockChain.add(new Block("First block", "0"));
+        validBlockChain.add(new Block("Second block", validBlockChain.get(validBlockChain.size() - 1).hash));
+        validBlockChain.add(new Block("Third block", validBlockChain.get(validBlockChain.size() - 1).hash));
+
+        assertThat(BlockChainUtil.isChainValid(validBlockChain)).isTrue();
+    }
+
+    @Test
+    public void shouldBlockChainBeInvalid() {
+
+        List<Block> validBlockChain = new ArrayList<Block>();
+        validBlockChain.add(new Block("First block", "0"));
+        validBlockChain.add(new Block("Second block", "@#%@$%J#$OGERNER"));
+        validBlockChain.add(new Block("Third block", validBlockChain.get(validBlockChain.size() - 1).hash));
+
+        assertThat(BlockChainUtil.isChainValid(validBlockChain)).isFalse();
+    }
+
+
+}
